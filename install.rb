0fa33292cf6638c210dcd28f0c493afbd10c0e91
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-03-26 03:47:04 -0700

    chmod +x install.rb

---
+++ install.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+irb_rc = File.join(ENV["HOME"], ".irbrc")
+
+if File.exist?(irb_rc)
+  warn "Failed to install: #{ irb_rc } already exists"
+else
+  system %W(ln -v #{ File.expand_path(".irbrc", File.dirname(__FILE__)) } #{ irb_rc })
+end
