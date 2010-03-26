3b007c0318bb306a9993b09a1270ec2e4d4dda46
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-03-26 14:25:41 +0300

    Add features, fix bugs

--- install.rb
+++ install.rb
@@ -5,5 +5,5 @@ irb_rc = File.join(ENV["HOME"], ".irbrc")
 if File.exist?(irb_rc)
   warn "Failed to install: #{ irb_rc } already exists"
 else
-  system *%W(ln -v #{ File.expand_path(".irbrc", File.dirname(__FILE__)) } #{ irb_rc })
+  system *%W(ln -s -v #{ File.expand_path(".irbrc", File.dirname(__FILE__)) } #{ irb_rc })
 end
